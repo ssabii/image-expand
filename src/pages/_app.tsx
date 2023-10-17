924c4cae9a594219dc65b902ab889c2f0a0be211
joseph <joseph@kmong.com>
2023-10-17 22:30:29 +0900

    feat: _app ViewTransition 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import ViewTransition from '@/components/ViewTransition'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ViewTransition>
+      <Component {...pageProps} />
+    </ViewTransition>
+  )
 }
