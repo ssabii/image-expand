6171b528bc97967cfe8a329f5ccf08b6125dd042
joseph <joseph@kmong.com>
2023-10-17 22:35:41 +0900

    feat: 이미지 도메인 등록

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['store.storeimages.cdn-apple.com'],
+  }
 }
 
 module.exports = nextConfig
